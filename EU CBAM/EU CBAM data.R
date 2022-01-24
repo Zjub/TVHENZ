8652a4b72a4997e9b8955c319a48b2c801049356
Zjub <Zjub@users.noreply.github.com>
2022-01-25 10:29:04 +1300

    Year data confirmed

--- EU CBAM/EU CBAM data.R
+++ EU CBAM/EU CBAM data.R
@@ -20,32 +20,68 @@ EUCBAMcat <- c(25231000,25232100,25232900,25239000,27160000,28080000,2814,283421
 #unzip("data.zip") # zip file is too large and so downloads corrupted.  Have to download amd umzip manually from your workspace.
 
 Importdata2020 <- read.csv("2020_Imports_HS10.csv")
+Importdata2019 <- read.csv("2019_Imports_HS10.csv")
+Importdata2018 <- read.csv("2018_Imports_HS10.csv")
 
-
+Exportdata2020 <- read.csv("2020_Exports_HS10.csv")
+Exportdata2019 <- read.csv("2019_Exports_HS10.csv")
+Exportdata2018 <- read.csv("2018_Exports_HS10.csv")
 
 # Drop everything with a leading zero (i.e. with 9 characters)
 
+#Importdata <- filter(Importdata, !(nchar(Harmonised.System.Code) < 10))
+
+Importdata2020 <- filter(Importdata2020, !(nchar(Harmonised.System.Code) < 10))
+Importdata2019 <- filter(Importdata2019, !(nchar(Harmonised.System.Code) < 10))
+Importdata2018 <- filter(Importdata2018, !(nchar(Harmonised.System.Code) < 10))
+Importdata <- rbind(Importdata2018,Importdata2019,Importdata2020)
+
+Exportdata2020 <- filter(Exportdata2020, !(nchar(Harmonised.System.Code) < 10))
+Exportdata2019 <- filter(Exportdata2019, !(nchar(Harmonised.System.Code) < 10))
+Exportdata2018 <- filter(Exportdata2018, !(nchar(Harmonised.System.Code) < 10))
+Exportdata <- rbind(Exportdata2018,Exportdata2019,Exportdata2020)
+
+# Select from each category - as there are no total categories the subsetting needs to be dependent on the length of the numeric string for category)
+
 for (i in 1:length(EUCBAMcat)){
-  nam <- paste("CBAMdata",i,sep="")
+  nam <- paste("CBAMdataImp",i,sep="")
   info <- Importdata[as.numeric(substr(Importdata$Harmonised.System.Code,1,nchar(EUCBAMcat[i]))) == EUCBAMcat[i],]
   assign(nam,info)
 }
 
-Cement <- rbind(CBAMdata1,CBAMdata2,CBAMdata3,CBAMdata4)
+for (i in 1:length(EUCBAMcat)){
+  nam <- paste("CBAMdataExp",i,sep="")
+  info <- Exportdata[as.numeric(substr(Importdata$Harmonised.System.Code,1,nchar(EUCBAMcat[i]))) == EUCBAMcat[i],]
+  assign(nam,info)
+}
 
-Electricity <- CBAMdata5
+CementImp <- rbind(CBAMdataImp1,CBAMdataImp2,CBAMdataImp3,CBAMdataImp4)
 
-Fertilisers <- rbind(CBAMdata6,CBAMdata7,CBAMdata8,CBAMdata9,CBAMdata10) # Note that 11 is subtracted from the total ,CBAMdata11
+ElectricityImp <- CBAMdataImp5
 
-Fertilisers <- filter(Fertilisers, !(Harmonised.System.Code %in% CBAMdata11$Harmonised.System.Code))
+FertilisersImp <- rbind(CBAMdataImp6,CBAMdataImp7,CBAMdataImp8,CBAMdataImp9,CBAMdataImp10) # Note that 11 is subtracted from the total ,CBAMdataImp11
 
-IronSteel <- rbind(CBAMdata12,CBAMdata15,CBAMdata16,CBAMdata17,CBAMdata18,CBAMdata19,CBAMdata20,CBAMdata21,CBAMdata22,CBAMdata23,CBAMdata24,CBAMdata25) # Note that CBAMdata13, CBAMdata14
+FertilisersImp <- filter(FertilisersImp, !(Harmonised.System.Code %in% CBAMdataImp11$Harmonised.System.Code))
 
-IronSteel <- filter(IronSteel, !(Harmonised.System.Code %in% CBAMdata13$Harmonised.System.Code))
-IronSteel <- filter(IronSteel, !(Harmonised.System.Code %in% CBAMdata14$Harmonised.System.Code))
+IronSteelImp <- rbind(CBAMdataImp12,CBAMdataImp15,CBAMdataImp16,CBAMdataImp17,CBAMdataImp18,CBAMdataImp19,CBAMdataImp20,CBAMdataImp21,CBAMdataImp22,CBAMdataImp23,CBAMdataImp24,CBAMdataImp25) # Note that CBAMdataImp13, CBAMdataImp14
 
-Aluminium <- rbind(CBAMdata26,CBAMdata27,CBAMdata28,CBAMdata29,CBAMdata30,CBAMdata31,CBAMdata32,CBAMdata33)
+IronSteelImp <- filter(IronSteelImp, !(Harmonised.System.Code %in% CBAMdataImp13$Harmonised.System.Code))
+IronSteelImp <- filter(IronSteelImp, !(Harmonised.System.Code %in% CBAMdataImp14$Harmonised.System.Code))
 
-# Select from each category - as there are no total categories the subsetting needs to be dependent on the length of the numeric string for category)
+AluminiumImp <- rbind(CBAMdataImp26,CBAMdataImp27,CBAMdataImp28,CBAMdataImp29,CBAMdataImp30,CBAMdataImp31,CBAMdataImp32,CBAMdataImp33)
 
-XXX
+CementExp <- rbind(CBAMdataExp1,CBAMdataExp2,CBAMdataExp3,CBAMdataExp4)
+
+ElectricityExp <- CBAMdataExp5
+
+FertilisersExp <- rbind(CBAMdataExp6,CBAMdataExp7,CBAMdataExp8,CBAMdataExp9,CBAMdataExp10) # Note that 11 is subtracted from the total ,CBAMdataExp11
+
+FertilisersExp <- filter(FertilisersExp, !(Harmonised.System.Code %in% CBAMdataExp11$Harmonised.System.Code))
+
+IronSteelExp <- rbind(CBAMdataExp12,CBAMdataExp15,CBAMdataExp16,CBAMdataExp17,CBAMdataExp18,CBAMdataExp19,CBAMdataExp20,CBAMdataExp21,CBAMdataExp22,CBAMdataExp23,CBAMdataExp24,CBAMdataExp25) # Note that CBAMdataExp13, CBAMdataExp14
+
+IronSteelExp <- filter(IronSteelExp, !(Harmonised.System.Code %in% CBAMdataExp13$Harmonised.System.Code))
+IronSteelExp <- filter(IronSteelExp, !(Harmonised.System.Code %in% CBAMdataExp14$Harmonised.System.Code))
+
+AluminiumExp <- rbind(CBAMdataExp26,CBAMdataExp27,CBAMdataExp28,CBAMdataExp29,CBAMdataExp30,CBAMdataExp31,CBAMdataExp32,CBAMdataExp33)
+
