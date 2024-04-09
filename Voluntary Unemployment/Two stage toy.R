918afb50c593bed4ef1f81b973892f7513266a03
Zjub <Zjub@users.noreply.github.com>
2024-04-09 14:07:00 +1000

    Update two stage toy
    Updated the toy to be usable as an example, will now write a new version as a function to vary by parameter.

--- Voluntary Unemployment/Two stage toy.R
+++ Voluntary Unemployment/Two stage toy.R
@@ -1,20 +1,26 @@
 # Define parameters
-L <- 0.5
-w1 <- 1
-w2 <- 1
-b <- 0.7
-delta <- 1
+L <- 0.5 # Probability of losing a job between periods 1 and 2
+w1 <- 1 # Wage of first period job
+w2 <- 1 # Wage of second period job
+b <- 0.7 # Unemployment benefit
+delta <- 1 # Discount rate
+C <- 0.1 # Fixed cost of search
+c <- 0.25 # Marginal cost of search
+cost_leave <- 1 # Set to 0 if the fixed cost of search is also removed when leaving.
 
 # Define the cost function to allow discontinuous choice for the fixed cost of work
-c_e <- function(e) {
-  if (e > 0) {
-    return(0.1 + 0.25 * e^2)
-  } else {
+c_e <- function(e,state) {
+  if (e > 0 & state == "stay") {
+    return(C + c * e^2)
+  } else if (e > 0 & state == "leave") {
+    return(C + 0 * e^2)
+  }
+  else{
     return(0)
   }
 }
 
-# Define a sequence of e values from 0 to 1
+# Define a sequence of e values from 0 to 1 - implicit is that the probability of reemployment is equal to these values (so a full effort of 1 implies a 100% probability of employment in period 2)
 e_values <- seq(0, 1, by = 0.01)
 
 # Calculate V_stay(e) and V_leave(e) for each e
@@ -23,8 +29,8 @@ V_leave <- numeric(length(e_values))
 
 for (i in 1:length(e_values)) {
   e <- e_values[i]
-  V_stay[i] <- w1 + (1 - L) * w2 + L * e * w2 - c_e(e)
-  V_leave[i] <- b + e * w2 - c_e(e)*0.0
+  V_stay[i] <- w1 + (1 - L) * w2 + L * e * w2 - c_e(e,"stay")
+  V_leave[i] <- b + e * w2 - c_e(e,"leave")*cost_leave
 }
 
 # Plot V_stay(e) and V_leave(e)
@@ -33,3 +39,20 @@ lines(e_values, V_leave, col = "red")
 legend("bottomright", legend = c("Stay", "Leave"), col = c("blue", "red"), lty = 1)
 
 # Optimisation is based on comparing the maximum value across both - but want to "print" based on whether the staying case maximises outcomes at 0 or at positive effort
+
+value_stay_nosearch <- V_stay[1]
+value_stay_search <- max(V_stay[2:length(V_stay)])
+stay_effort_value <- e_values[which.max(V_stay)]
+value_leave <- max(V_leave)
+leave_effort_value <- e_values[which.max(V_leave)]
+
+choice_value <- max(value_stay_nosearch, value_stay_search, value_leave)
+
+if (choice_value == value_stay_nosearch) {
+  print(paste("The maximum utility value is", choice_value, "and it is associated with staying without search."))
+} else if (choice_value == value_stay_search) {
+  print(paste("The maximum utility value is", choice_value, "and it is associated with staying with search and", stay_effort_value,"search effort."))
+} else if (choice_value == value_leave) {
+  print(paste("The maximum utility value is", choice_value, "and it is associated with leaving and", leave_effort_value,"search effort."))
+}
+
