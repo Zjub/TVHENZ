44882c23f4d2f745ccc743d1bc40b9993cc7a342
Zjub <Zjub@users.noreply.github.com>
2024-10-02 17:20:37 +1000

    Updating game

--- MF coordination/SI_game/two_stage_game.R
+++ MF coordination/SI_game/two_stage_game.R
@@ -21,38 +21,45 @@ set.seed(123)
 
 # Parameters for the model
 gamma <- 1.5  # Intercept for inflation [setting this below alpha creates a trade-off where inflation is lower than output]
-alpha <- 2    # Intercept for output
-bG <- 0.7     # Sensitivity of output to fiscal policy
+alpha <- 1.5    # Intercept for output
+bG <- 0.5     # Sensitivity of output to fiscal policy
 bB <- 0.5     # Sensitivity of output to monetary policy
 dG <- 0.5     # Sensitivity of inflation to fiscal policy
 dB <- 0.5     # Sensitivity of inflation to monetary policy
+eG <- 0.5     # Penalty factor on change in fiscal policy
+eB <- 0.5     # Penalty factor on change in monetary policy
 
 # New separate weights for inflation in the loss functions
 mu_G <- 0  # Government's weight on inflation
 mu_B <- 1  # Central bank's weight on inflation
+theta_G_star <- 0.5 # Government's weight on instrument
+theta_B_star <- 0.5 # Central bank's weight on instrument
 
-# Target inflation and output
-pi_G_star <- 2
-pi_B_star <- 2
-y_G_star <- 2
-y_B_star <- 2
+# Target inflation and output and instrument
+pi_G_star <- 2 # Government's target inflation
+pi_B_star <- 2 # Central bank's target inflation
+y_G_star <- 2 # Government's target output
+y_B_star <- 2 # Central bank's target output
+beta_G_star <- 0 # Government's preferred f
+beta_B_star <- 0 # Central bank's preferred m
 
-# Precommitment costs - not used
-cost_G_precommit <- 0.5
-cost_B_precommit <- 0.5
+
+# Precommitment costs - not used, using the preference beta's to start. Transition to this idea
+# cost_G_precommit <- 0.5
+# cost_B_precommit <- 0.5
 
 # Government's best response to monetary policy choice by the central bank
 BR_G <- function(m) {
-  numerator <- -(bG * (1 - mu_G) * (y_G_star - alpha + bB*m) + dG * mu_G * (pi_G_star - gamma + dB*m))
-  denominator <- ((bG^2) * (1 - mu_G) + (dG^2) * mu_G)
+  numerator <- -(bG * (1 - mu_G) * (y_G_star - alpha + bB*m) + dG * mu_G * (pi_G_star - gamma + dB*m) - theta_G_star*eG*beta_G_star) 
+  denominator <- ((bG^2) * (1 - mu_G) + (dG^2) * mu_G + theta_G_star*eG)
   f_star <- numerator / denominator
   return(f_star)
 }
 
 # Central bank's best response to fiscal policy choice by the government
 BR_B <- function(f) {
-  numerator <- -(bB * (1 - mu_B) * (y_B_star - alpha + bG*f) + dB * mu_B * (pi_B_star - gamma + dG*f))
-  denominator <- ((bB^2) * (1 - mu_B) + (dB^2) * mu_B)
+  numerator <- -(bB * (1 - mu_B) * (y_B_star - alpha + bG*f) + dB * mu_B * (pi_B_star - gamma + dG*f) - theta_B_star*eB*beta_B_star) 
+  denominator <- ((bB^2) * (1 - mu_B) + (dB^2) * mu_B + theta_B_star*eB)
   m_star <- numerator / denominator
   return(m_star)
 }
@@ -71,7 +78,7 @@ plot_df_cb <- data.frame(f_values = f_values, m_values_cb = m_values_cb)
 # Plot the best response functions
 ggplot() +
   geom_line(data = plot_df_gov, aes(x = m_values, y = f_values_gov), color = "blue", size = 1, linetype = "solid") +
-  geom_line(data = plot_df_cb, aes(x = f_values, y = m_values_cb), color = "red", size = 1, linetype = "dashed") +
+  geom_line(data = plot_df_cb, aes(x = m_values_cb, y = f_values), color = "red", size = 1, linetype = "dashed") +
   labs(x = "Monetary Policy (m)", y = "Fiscal Policy (f)", 
        title = "Best Response Functions: Government vs. Central Bank") +
   annotate("text", x = 0, y = 0, label = "Intersection = Nash Equilibrium", vjust = -1, hjust = -0.5) +
@@ -142,7 +149,8 @@ precommitment_government <- function() {
   effective_loss_G <- function(f) {
     m_star <- BR_B(f)
     L_G <- mu_G * (pi_G_star - (gamma - dG * f - dB * m_star))^2 + 
-      (1 - mu_G) * (y_G_star - (alpha - bG * f - bB * m_star))^2
+      (1 - mu_G) * (y_G_star - (alpha - bG * f - bB * m_star))^2 +
+      theta_G_star*(beta_G_star - eG*f)^2
     return(L_G)
   }
   
@@ -157,7 +165,8 @@ precommitment_central_bank <- function() {
   effective_loss_B <- function(m) {
     f_star <- BR_G(m)
     L_B <- mu_B * (pi_B_star - (gamma - dG * f_star - dB * m))^2 + 
-      (1 - mu_B) * (y_B_star - (alpha - bG * f_star - bB * m))^2
+      (1 - mu_B) * (y_B_star - (alpha - bG * f_star - bB * m))^2 +
+      theta_B_star*(beta_B_star - eB*m)^2
     return(L_B)
   }
   
@@ -194,15 +203,15 @@ precommit_result_both_independent <- precommit_both_independent()
 precommit_outcomes_both_independent <- compute_inflation_output(precommit_result_both_independent$fiscal, precommit_result_both_independent$monetary)
 
 # Cooperative outcome: maximizing joint welfare
-cooperative_utility <- function(f, m) {
-  uG <- -mu_G * (pi_G_star - (gamma - dG * f - dB * m))^2 - (1 - mu_G) * (y_G_star - (alpha - bG * f - bB * m))^2
-  uB <- -mu_B * (pi_B_star - (gamma - dG * f - dB * m))^2 - (1 - mu_B) * (y_B_star - (alpha - bG * f - bB * m))^2
+utility <- function(f, m) {
+  uG <- -(mu_G * (pi_G_star - (gamma - dG * f - dB * m))^2 + (1 - mu_G) * (y_G_star - (alpha - bG * f - bB * m))^2 + theta_G_star*(beta_G_star - eG*f)^2)
+  uB <- -(mu_B * (pi_B_star - (gamma - dG * f - dB * m))^2 + (1 - mu_B) * (y_B_star - (alpha - bG * f - bB * m))^2 + theta_B_star*(beta_B_star - eB*m)^2)
   return(uG + uB)
 }
 
 cooperative_opt <- optimx(
   par = c(f = 0, m = 0), 
-  fn = function(x) -cooperative_utility(x[1], x[2]),
+  fn = function(x) -utility(x[1], x[2]),
   method = "BFGS"
 )
 f_cooperative <- cooperative_opt$f
@@ -215,7 +224,8 @@ outcomes_df <- data.frame(
   Fiscal_Policy = c(nash_result$fiscal, precommit_result_gov$fiscal, precommit_result_cb$fiscal, precommit_result_both_independent$fiscal, f_cooperative),
   Monetary_Policy = c(nash_result$monetary, precommit_result_gov$monetary, precommit_result_cb$monetary, precommit_result_both_independent$monetary, m_cooperative),
   Output = c(nash_outcomes$output, precommit_outcomes_gov$output, precommit_outcomes_cb$output, precommit_outcomes_both_independent$output, cooperative_outcomes$output),
-  Inflation = c(nash_outcomes$inflation, precommit_outcomes_gov$inflation, precommit_outcomes_cb$inflation, precommit_outcomes_both_independent$inflation, cooperative_outcomes$inflation)
+  Inflation = c(nash_outcomes$inflation, precommit_outcomes_gov$inflation, precommit_outcomes_cb$inflation, precommit_outcomes_both_independent$inflation, cooperative_outcomes$inflation),
+  Total_Utility = c(utility(nash_result$fiscal,nash_result$monetary),utility(precommit_result_gov$fiscal,precommit_result_gov$monetary),utility(precommit_result_cb$fiscal,precommit_result_cb$monetary),utility(precommit_result_both_independent$fiscal,precommit_result_both_independent$monetary),utility(f_cooperative,m_cooperative))
 )
 
 # Reshape fiscal and monetary policy data for graphs
@@ -231,6 +241,12 @@ ggplot(policy_df_long, aes(x = Scenario, y = Value, fill = Policy_Type)) +
   theme_minimal()
 
 #save_e61("Relative_Looseness.png", pad_width = 1, res = 2)
+
+ggplot(policy_df_long, aes(x = Scenario, y = Total_Utility, fill = Policy_Type)) +
+  geom_bar(stat = "identity", position = "dodge", width = 0.6) +
+  labs(title = "Loss Across Scenarios", 
+       y = "Policy Looseness", x = "Scenario") +
+  theme_minimal()
 
 # Reshape output and inflation data for graphs
 outcome_df_long <- outcomes_df %>%
