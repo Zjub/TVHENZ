2f4e66363ad5c29c0ff4fa4d5d29d451b8b3f9a2
Zjub <Zjub@users.noreply.github.com>
2025-06-06 06:03:01 +1000

    update fiscal

--- e61 Projects/Fiscal sustainability/Election newsletter/Debt_scenarios.R
+++ e61 Projects/Fiscal sustainability/Election newsletter/Debt_scenarios.R
@@ -20,15 +20,15 @@ library(mFilter)
 rm(list=ls())
 gc()
 
-debt_dt <- read_excel("Debt scenario.xlsx")
+debt_dt <- read_excel("Debt scenario2.xlsx")
 
 setDT(debt_dt)
 
-debt_long <- melt(debt_dt, id.vars = "Year",
+debt_long_rev <- melt(debt_dt[,.(Year,Forecast,Mig_Prod_TOT,Mig_Prod)], id.vars = "Year",
                           variable.name = "Type",
                           value.name = "value")
 
-ggplot(debt_long,aes(x=Year,y=value,colour=Type)) + geom_line() +
+ggplot(debt_long_rev,aes(x=Year,y=value,colour=Type)) + geom_line() +
   scale_y_continuous_e61(limits = c(30,70)) +
   plab(c("Forecast","Productivity and TOT shock","Lower Productivity"),x=c(2024,2024,2024),y=c(66,58,62)) +
   labs_e61(#title = "Gross debt projections",
@@ -37,15 +37,52 @@ ggplot(debt_long,aes(x=Year,y=value,colour=Type)) + geom_line() +
            x="",
            sources = c("PBO","e61"))
 
-ggplot(debt_long,aes(x=Year,y=value,colour=Type)) + geom_line() +
+ggplot(debt_long_rev,aes(x=Year,y=value,colour=Type)) + geom_line() +
   scale_y_continuous_e61(limits = c(30,70)) +
   plab(c("Forecast","Productivity and TOT shock","Lower Productivity"),x=c(2024,2024,2024),y=c(66,58,62)) +
-  labs_e61(title = "Gross debt projections",
+  labs_e61(title = "Gross debt projections (Revenue shocks)",
     subtitle = "% of nominal GDP",
     y="%",
     x="",
     sources = c("PBO","e61"),
     footnotes = c("Scenario reduces net migration and productivity, alongside lower real wage growth to match. Terms of trade shock reflects a 45% decline in key export prices."))
+
+save_e61("Projections_debt_rev.png",res=2)
+
+## Do expenses
+
+debt_long_exp <- melt(debt_dt[,.(Year,Forecast,Defence,Defence_NDIS,Defence_interest_NDIS)], id.vars = "Year",
+                      variable.name = "Type",
+                      value.name = "value")
+
+
+ggplot(debt_long_exp,aes(x=Year,y=value,colour=Type)) + geom_line() +
+  scale_y_continuous_e61(limits = c(30,70)) +
+  plab(c("Forecast","Defence spending","+ NDIS","+ Interest increase"),x=c(2024,2024,2024,2024),y=c(66,62,58,55)) +
+  labs_e61(title = "Gross debt projections (expenditure shock)",
+    subtitle = "% of nominal GDP",
+    y="% GDP",
+    x="",
+    sources = c("PBO","e61"))
+
+save_e61("Projections_debt_exp.png",res=2)
+
+## Overall
+
+debt_long <- melt(debt_dt[,.(Year,Forecast,Mig_Prod_TOT,Defence_interest_NDIS,Expenditure_Revenue)], id.vars = "Year",
+                      variable.name = "Type",
+                      value.name = "value")
+
+
+ggplot(debt_long,aes(x=Year,y=value,colour=Type)) + geom_line() +
+  scale_y_continuous_e61(limits = c(30,90)) +
+  plab(c("Forecast","Expenditure Shock","Revenue Shock","Both shocks"),x=c(2024,2024,2024,2024),y=c(82,77,72,67)) +
+  labs_e61(title = "Gross debt projections",
+           subtitle = "% of nominal GDP",
+           y="%",
+           x="",
+           sources = c("PBO","e61"),
+           )
 
 save_e61("Projections_debt.png",res=2)
 
